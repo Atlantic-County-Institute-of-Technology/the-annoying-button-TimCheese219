6c1a54eaae24237deb85a00ca547cd72fadba064
TimCheese219 <sour1072@student.acitech.org>
2026-03-16 08:33:37 -0400

    Finally Done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let score = document.getElementById("score");
 let cookie_button = document.getElementById("cookie_button");
 let main_button = document.getElementById("main_button")
 const target_url =  "https://www.youtube.com/watch?v=wKjIYlaSnEA&list=PLJhVtn7lafvaN3H8IZgqodL90RUckBU9C"
-const video_url = "https://www.youtube.com/watch?v=lEjTEYsW_kU"
+
 
 var clicks = 0;
 var click_power = 1;
@@ -24,26 +24,23 @@ function check_score() {
     if( clicks == 100)
         make_grandma();
 
-    if (clicks >= 100 && clicks < 101 ) {
+    if (clicks >= 100 && clicks < 200 ) {
         cookie_button.style.backgroundColor = 'blue';
         alert("dude, ya FFFFFFEEEEEEEEELLLLLLLL MMMMMMEEEEEEEEEE!!!!!!!!!!!")
     }
-    else if (clicks >= 200 && clicks < 301 ) {
+    else if (clicks >= 200 && clicks < 300 ) {
         cookie_button.style.backgroundColor = 'orange';
         alert("Hey that is not nice")
     }
-    else if (clicks >= 400 && clicks < 501 ) {
+    else if (clicks >= 300 && clicks < 400 ) {
         cookie_button.style.backgroundColor = 'green';
-        ("Stoppp, I will bring the BOSS OUT!!!")
+        alert("Stoppp, I will bring the BOSS OUT!!!")
     }
-    else if (clicks >= 600 && clicks < 701) {
+    else if (clicks >= 400 && clicks < 500 ) {
         cookie_button.style.backgroundColor = 'brown';
     }
     if (clicks >= 450 && clicks < 500) {
         window.open (target_url, '_blank')
-    }
-    if(clicks >= 1001 && clicks < 2000) {
-        window.open (video_url, '_blank')
     }
     if (clicks == 100) {
         cookie_button.style.backgroundColor = 'blue';
